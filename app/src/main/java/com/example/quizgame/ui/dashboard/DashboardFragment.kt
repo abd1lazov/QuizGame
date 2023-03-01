07b833b1d524e8216534b3d32415668cb93782fc
abdykadyr <abdukadyrabdilazov@icloud.com>
2023-03-01 20:53:55 +0100

    Initial commit

---
+++ app/src/main/java/com/example/quizgame/ui/dashboard/DashboardFragment.kt
@@ -0,0 +1,35 @@
+package com.example.quizgame.ui.dashboard
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.quizgame.databinding.FragmentHistoryBinding
+
+class DashboardFragment : Fragment() {
+
+    private var _binding: FragmentHistoryBinding? = null
+
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val dashboardViewModel =
+            ViewModelProvider(this).get(DashboardViewModel::class.java)
+
+        _binding = FragmentHistoryBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/example/quizgame/ui/home/HomeViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.quizgame.ui.home
+
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+}
